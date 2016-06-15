898c93717efa6b4237ea0c0857643f1a84ca2699
Wayne Ngan <wayne.ngan@gmail.com>
2016-06-14 23:55:27 -0700

    website import

---
+++ email_func.js
@@ -0,0 +1,17 @@
+function pomegranate()
+{
+    var str = "mai" + String.fromCharCode(108) + String.fromCharCode(116) + String.fromCharCode(111) + ":";
+    str = str + "nga" + String.fromCharCode(110) + String.fromCharCode(64);
+    str = str + "astro.utoronto.ca";
+    
+    window.location.href = str;
+}
+
+function watermelon()
+{
+    var str = "nga" + String.fromCharCode(110) + String.fromCharCode(64);
+    str = str + "astro.utoronto.ca";
+    
+    document.write(str);
+
+}

59c0a0b7cd7e0098142a695911696cfcd46e3e7e
Wayne Ngan <wayne.ngan@gmail.com>
2016-06-23 02:34:38 -0400

    use new email, change og:url to new domain

--- email_func.js
+++ email_func.js
@@ -1,17 +1,32 @@
 function pomegranate()
 {
-    var str = "mai" + String.fromCharCode(108) + String.fromCharCode(116) + String.fromCharCode(111) + ":";
+    /*var str = "mai" + String.fromCharCode(108) + String.fromCharCode(116) + String.fromCharCode(111) + ":";
     str = str + "nga" + String.fromCharCode(110) + String.fromCharCode(64);
-    str = str + "astro.utoronto.ca";
+    str = str + "astro.utoronto.ca";*/
+
+    var str = "mai" + String.fromCharCode(108) + String.fromCharCode(116) + String.fromCharCode(111) + ":" + tomato();
     
     window.location.href = str;
 }
 
 function watermelon()
 {
-    var str = "nga" + String.fromCharCode(110) + String.fromCharCode(64);
+    /*var str = "nga" + String.fromCharCode(110) + String.fromCharCode(64);
     str = str + "astro.utoronto.ca";
-    
-    document.write(str);
+
+    document.write(str);*/
+
+    document.write(tomato());
+
+}
+
+function tomato()
+{
+	var str = "wayn" + String.fromCharCode(101) + String.fromCharCode(64);
+	str = str + "wayn" + String.fromCharCode(101);
+	str = str + "nga" + String.fromCharCode(110);
+	str = str + ".info";
+
+	return str;
 
 }
